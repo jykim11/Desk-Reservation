bc2ebc30c350a7e3212a3ecb914cffa97f998a20
Kailash Muthu <kailashm2212@gmail.com>
2023-04-02 18:01:11 -0400

    15 api services (#20)
    * added api endpoint and template service
    
    * fixed api naming convention and completed service
    
    * added reservation route to main.py
    
    * fixed api parameter errors and added authentication

---
+++ backend/api/reservation.py
@@ -0,0 +1,17 @@
+"""Reservation API
+
+This API is used to modify available resources."""
+
+from fastapi import APIRouter, Depends, HTTPException
+from ..models import User
+from ..services import ResService, UserPermissionError
+from .authentication import registered_user
+
+api = APIRouter(prefix="/api/reservation")
+
+@api.get("", tags=['Reservation'])
+def list_desks(subject: User = Depends(registered_user), desk_res: ResService = Depends()):
+    try:
+        return desk_res.listdesks(subject)
+    except UserPermissionError as e:
+        raise HTTPException(status_code=403, detail=str(e))
---
+++ backend/services/reservation.py
@@ -0,0 +1,18 @@
+from fastapi import Depends
+from sqlalchemy import select, or_, func
+from sqlalchemy.orm import Session
+from ..database import db_session
+from ..models import User, Desk, Paginated, PaginationParams
+from ..entities import UserEntity, DeskEntity
+from .permission import PermissionService
+
+class ResService:
+    def __init__(self, session: Session = Depends(db_session), permission: PermissionService = Depends()):
+        self._session = session
+        self._permission = permission
+
+    def listdesks(self, subject: User) -> list[Desk]:
+        self._permission.enforce(subject, 'reservation.listdesks', 'reservation/')
+        stmt = select(DeskEntity).order_by(DeskEntity.id)
+        desk_entities = self._session.execute(stmt).scalars()
+        return [desk_entity.to_model() for desk_entity in desk_entities]
